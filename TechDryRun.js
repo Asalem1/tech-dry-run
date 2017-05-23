beed5d35fad7d153c2e5581b4ee6ef30314aceb8
Ariel <Ariel.Salem1989@gmail.com>
2017-05-22 20:16:39 -0700

    first functional attempt

--- TechDryRun.js
+++ TechDryRun.js
@@ -1,8 +1,17 @@
 //Tech Dry Run
 
 var hrr24 = {
-  name: 'Ariel Salem'
+  name: 'Scott Rudiger'
 }
 
+//can you see this
 
-
+//David Berry was here *_*
+//Himanshu was here *_*
+//Mark Suyat was here
+//Scott Rudiger was here. :D
+//hey Scott!
+//kenneth
+//Hey this is Doug!
+// Bongani Mashele was here.
+// William
